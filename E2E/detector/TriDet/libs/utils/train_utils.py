3061263e4e3bc4da8d4827a8da4b47f1208dfe8e
maurizio <mauriziopapa52@gmail.com>
2024-03-17 17:56:17 +0100

    update utils

--- E2E/detector/TriDet/libs/utils/train_utils.py
+++ E2E/detector/TriDet/libs/utils/train_utils.py
@@ -90,8 +90,8 @@ def make_optimizer(model, optimizer_config):
     param_dict = {pn: p for pn, p in model.named_parameters()}
     inter_params = decay & no_decay
     union_params = decay | no_decay
-    assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
-    assert len(param_dict.keys() - union_params) == 0, \
+    #assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params),)
+    #assert len(param_dict.keys() - union_params) == 0, \
         "parameters %s were not separated into either decay/no_decay set!" \
         % (str(param_dict.keys() - union_params),)
 
